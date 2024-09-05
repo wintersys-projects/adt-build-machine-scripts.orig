2fb6d951bd5cb99d6f545002170e2fb2e7eadf60
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-09-05 20:32:16 +0100

    Update CreateInitialVPC.sh

--- providerscripts/server/CreateInitialVPC.sh
+++ providerscripts/server/CreateInitialVPC.sh
@@ -76,6 +76,8 @@ then
 		then
 			status "It looks like your build machine couldn't be connected to the Private Network, please investigate"
 			exit
+   		else
+			. ${BUILD_HOME}/helperscripts/SetupEth1.sh
 		fi
 	fi  
 fi
