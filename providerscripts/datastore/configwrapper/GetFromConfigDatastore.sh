17dceca01948980750d9aeb698c592573e81faa4
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-19 18:33:41 +0100

    Update GetFromConfigDatastore.sh

--- providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh
@@ -28,21 +28,5 @@ configbucket="${configbucket}-config"
 
 if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "s3cmd" ] )
 then
-  if ( [ -f ${2} ] )
-	then
-	  /usr/bin/s3cmd get s3://${configbucket}/$2 $3 
-	elif ( [ -f ./${2} ] )
-	then
-	  /usr/bin/s3cmd get s3://${configbucket}/$2 $3
-		/bin/rm ./$2
-	elif ( [ -f /tmp/${2} ] )
-	then
-	  /usr/bin/s3cmd get s3://${configbucket}/$2 /tmp/$3 
-	else
-	  directory="`/bin/echo ${1} | /usr/bin/awk -F'/' 'NF{NF-=1};1' | /bin/sed 's/ /\//g'`"
-		/bin/mkdir -p /tmp/${directory}
-		/bin/touch /tmp/$2
-		/usr/bin/s3cmd get s3://${configbucket}/$2 /tmp/$3
-		/bin/rm /tmp/$2
-	fi
+	/usr/bin/s3cmd get s3://${configbucket}/$2 $3 
 fi
