7979489d90bbd0c6299ed61193ee4e260a27e403
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-19 19:54:11 +0100

    Update PerformStandardBuildChain.sh

--- buildscripts/PerformStandardBuildChain.sh
+++ buildscripts/PerformStandardBuildChain.sh
@@ -348,8 +348,11 @@ else
 	. ${BUILD_HOME}/providerscripts/security/firewall/TightenBuildMachineFirewall.sh
 	export CLOUDHOST="${cloudhost_holder}"
 
-	${BUILD_HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh ${WEBSITE_URL} configuration.php.default  ${BUILD_HOME}/buildconfiguration
- 
+	while ( [ ! -f ${BUILD_HOME}/buildconfiguration/configuration.php.default ] )
+ 	do
+		${BUILD_HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh ${WEBSITE_URL} configuration.php.default  ${BUILD_HOME}/buildconfiguration
+ 		/bin/sleep 10
+   	done
 	##Do the build finalisation procedures
 	. ${BUILD_HOME}/buildscripts/FinaliseBuildProcessing.sh
 fi
