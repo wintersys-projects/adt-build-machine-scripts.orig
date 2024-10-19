b602c3608578ad193bdeb52f5d26f9447ecf86fc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-19 20:49:51 +0100

    Update PerformStandardBuildChain.sh

--- buildscripts/PerformStandardBuildChain.sh
+++ buildscripts/PerformStandardBuildChain.sh
@@ -356,10 +356,26 @@ else
 		${BUILD_HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh ${WEBSITE_URL} configuration.php.default  ${BUILD_HOME}/buildconfiguration
  		/bin/sleep 10
    	done
+    
 	if ( [ -f ${BUILD_HOME}runtimedata/linode/DBaaS_HOSTNAME ] )
  	then
-  		db_hostname="`/bin/cat ${BUILD_HOME}runtimedata/${CLOUDHOST}/DBaaS_HOSTNAME`"
+  		DB_HOSTNAME="`/bin/cat ${BUILD_HOME}runtimedata/${CLOUDHOST}/DBaaS_HOSTNAME`"
     	fi
+
+     	if ( [ "${DB_HOSTNAME}" = "" ] )
+      	then
+      		if ( [ "${DBIP_PRIVATE}" = "" ] )
+  		then
+     			DBIP_PRIVATE="`/bin/ls ${BUILD_HOME}/runtimedata/ips/${CLOUDHOST}/${BUILD_IDENTIFIER}/DBPRIVATEIP:* | /usr/bin/awk -F':' '{print $NF}'`"
+		fi
+ 
+		if ( [ "${DBIP}" = "" ] )
+  		then
+     			DBIP="`/bin/ls ${BUILD_HOME}/runtimedata/ips/${CLOUDHOST}/${BUILD_IDENTIFIER}/DBIP:* | /usr/bin/awk -F':' '{print $NF}'`"
+		fi
+  	fi
+
+   	
 
      	############ADDED
       
