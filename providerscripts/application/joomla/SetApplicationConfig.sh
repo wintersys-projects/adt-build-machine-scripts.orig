d0bc7d22222e0104a071c563363f42e5121dbf1a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-20 17:00:23 +0100

    Update SetApplicationConfig.sh

--- providerscripts/application/joomla/SetApplicationConfig.sh
+++ providerscripts/application/joomla/SetApplicationConfig.sh
@@ -26,11 +26,20 @@ secret="`${BUILD_HOME}/providerscripts/datastore/configwrapper/ListFromConfigDat
  	
 /bin/sed -i "/\$dbprefix /c\        public \$dbprefix = \'${dbprefix}\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
 /bin/sed -i "/\$secret /c\        public \$secret = \'${secret}\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
-/bin/sed -i "/\$dbtype /c\        public \$dbtype = \'mysqli\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
-/bin/sed -i "/\$host = /c\   public \$host = \'${db_identifier}:${DB_PORT}\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
 /bin/sed -i "/\$user/c\       public \$user = \'${database_username}\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
 /bin/sed -i "/\$password/c\   public \$password = \'${database_password}\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
 /bin/sed -i "/\$db /c\        public \$db = \'${database_name}\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
+
+if ( [ "${DATABASE_INSTALLATION_TYPE}" = "Postgres" ] )
+then
+	/bin/sed -i "/\$dbtype /c\        public \$dbtype = \'pgsql\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
+	/bin/sed -i "/\$port /d" ${BUILD_HOME}/buildconfiguration/configuration.php.default
+	/bin/sed -i "/\$host /c\        public \$host = \'${db_identifier}\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
+	/bin/sed -i "/\$host /a        public \$port = \'${DB_PORT}\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
+else
+ /bin/sed -i "/\$dbtype /c\        public \$dbtype = \'mysqli\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
+ /bin/sed -i "/\$host = /c\   public \$host = \'${db_identifier}:${DB_PORT}\';" ${BUILD_HOME}/buildconfiguration/configuration.php.default
+fi
 
 ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${WEBSITE_URL} ${BUILD_HOME}/buildconfiguration/configuration.php.default joomla_configuration.php
 
