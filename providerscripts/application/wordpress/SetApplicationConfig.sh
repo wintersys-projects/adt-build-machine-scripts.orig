21f3f3a1ffaa8447ef8aafcf69e119fb33da63e9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-20 16:49:33 +0100

    Update SetApplicationConfig.sh

--- providerscripts/application/wordpress/SetApplicationConfig.sh
+++ providerscripts/application/wordpress/SetApplicationConfig.sh
@@ -1,17 +1,17 @@
 
 dbprefix="`${BUILD_HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh ${WEBSITE_URL} DBPREFIX:* | /usr/bin/awk -F':' '{print $NF}'`"
 
-/bin/sed -i "/DB_HOST/c\ define('DB_HOST', \"${db_identifier}:${DB_PORT}\");" ${HOME}/runtime/wordpress_config.php
-/bin/sed -i "/DB_USER/c\ define('DB_USER', \"${database_username}\");" ${HOME}/runtime/wordpress_config.php
-/bin/sed -i "/DB_PASSWORD/c\ define('DB_PASSWORD', \"${database_password}\");" ${HOME}/runtime/wordpress_config.php
-/bin/sed -i "/DB_NAME/c\ define('DB_NAME', \"${database_name}\");" ${HOME}/runtime/wordpress_config.php
-/bin/sed -i "/\$table_prefix/c\ \$table_prefix=\"${dbprefix}\";" ${HOME}/runtime/wordpress_config.php
+/bin/sed -i "/DB_HOST/c\ define('DB_HOST', \"${db_identifier}:${DB_PORT}\");" ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
+/bin/sed -i "/DB_USER/c\ define('DB_USER', \"${database_username}\");" ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
+/bin/sed -i "/DB_PASSWORD/c\ define('DB_PASSWORD', \"${database_password}\");" ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
+/bin/sed -i "/DB_NAME/c\ define('DB_NAME', \"${database_name}\");" ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
+/bin/sed -i "/\$table_prefix/c\ \$table_prefix=\"${dbprefix}\";" ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
 
-/bin/sed -i "/'AUTH_KEY'/i XXYYZZ" ${HOME}/runtime/wordpress_config.php
-/bin/sed -i '/AUTH_KEY/,+7d' ${HOME}/runtime/wordpress_config.php
+/bin/sed -i "/'AUTH_KEY'/i XXYYZZ" ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
+/bin/sed -i '/AUTH_KEY/,+7d' ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
 salts="`/usr/bin/curl https://api.wordpress.org/secret-key/1.1/salt`"
-/bin/sed -n '/XXYYZZ/q;p' ${HOME}/runtime/wordpress_config.php > /tmp/firsthalf
-/bin/sed '0,/^XXYYZZ$/d' ${HOME}/runtime/wordpress_config.php > /tmp/secondhalf
+/bin/sed -n '/XXYYZZ/q;p' ${BUILD_HOME}/buildconfiguration/wp-config-sample.php > /tmp/firsthalf
+/bin/sed '0,/^XXYYZZ$/d' ${BUILD_HOME}/buildconfiguration/wp-config-sample.php > /tmp/secondhalf
 /bin/cat /tmp/firsthalf > /tmp/fullfile
 /bin/echo ${salts} >> /tmp/fullfile
 /bin/echo "/* SALTEDALREADY */" >> /tmp/fullfile
@@ -24,5 +24,5 @@ salts="`/usr/bin/curl https://api.wordpress.org/secret-key/1.1/salt`"
 /bin/echo "define('DISABLE_WP_CRON', true);" >> /tmp/fullfile
 /bin/cat /tmp/secondhalf >> /tmp/fullfile
 /bin/rm /tmp/firsthalf /tmp/secondhalf
-/bin/mv /tmp/fullfile ${HOME}/runtime/wordpress_config.php
+/bin/mv /tmp/fullfile ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
 
