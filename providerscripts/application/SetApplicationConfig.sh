402ff1b1fb5b62cf32161213c589aa2b0438c2a9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-20 12:09:12 +0100

    Update SetApplicationConfig.sh

--- providerscripts/application/SetApplicationConfig.sh
+++ providerscripts/application/SetApplicationConfig.sh
@@ -1,10 +1,6 @@
  	#################ADDED
 
-	while ( [ ! -f ${BUILD_HOME}/buildconfiguration/configuration.php.default ] )
- 	do
-		${BUILD_HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh ${WEBSITE_URL} configuration.php.default  ${BUILD_HOME}/buildconfiguration
- 		/bin/sleep 10
-   	done
+
     
 	if ( [ -f ${BUILD_HOME}runtimedata/linode/DBaaS_HOSTNAME ] )
  	then
