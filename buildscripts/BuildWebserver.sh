da7c2f65b2aa072feac15f319a47f65dc5d66682
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-21 22:19:50 +0100

    Update BuildWebserver.sh

--- buildscripts/BuildWebserver.sh
+++ buildscripts/BuildWebserver.sh
@@ -72,9 +72,7 @@ do
 	counter="`/usr/bin/expr ${counter} + 1`"
 	status "OK... Building a webserver. This is the ${counter} attempt of 5"
 	WEBSITE_IDENTIFIER="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`"
-
- >&2 echo "XXXXX 1"
-
+ 
 	#Check if there is a webserver already running. If there is, then skip building the webserver
 	if ( [ "`${BUILD_HOME}/providerscripts/server/NumberOfServers.sh "webserver" ${CLOUDHOST} 2>/dev/null`" -eq "0" ] )
 	then
@@ -93,8 +91,8 @@ do
 		fi
 
 		status "Initialising a new server machine, please wait......"
->&2 echo "XXXXX 2"
-		server_started="0"
+
+  		server_started="0"
 		while ( [ "${server_started}" = "0" ] )
 		do
 			count="0"
@@ -119,8 +117,7 @@ do
 			ip=""
 			private_ip=""
 			count="0"
- >&2 echo "XXXXX 3"
-
+   
 			#Keep trying until we get the ip addresses of our new machine, both public and private ips
 			while ( ( [ "${ip}" = "" ] || [ "${private_ip}" = "" ] ) || [ "${ip}" = "0.0.0.0" ] && [ "${count}" -lt "20" ] )
 			do
@@ -138,8 +135,7 @@ do
 				status "I haven't been able to start your server, trying again...."
 			fi
 	   done
- >&2 echo "XXXXX 4"
-
+    
 	   ${BUILD_HOME}/providerscripts/server/EnsureServerAttachedToVPC.sh "${CLOUDHOST}" "${webserver_name}" "${private_ip}"
 
 		WSIP=${ip}
@@ -170,9 +166,7 @@ do
 		fi
 		
 		/usr/bin/ssh-keyscan -T 60 ${ws_active_ip} >> ${WEBSERVER_PUBLIC_KEYS}
-
-   >&2 echo "XXXXX 5"
-
+  
 		keytry="0"
 		while ( [ "`/usr/bin/diff -s /dev/null ${WEBSERVER_PUBLIC_KEYS} | /bin/grep identical`" != "" ] && [ "${keytry}" -lt "10" ] )
 		do
@@ -223,8 +217,6 @@ do
 				/bin/sleep 5
 				gitfetchno="`/usr/bin/expr ${gitfetchno} + 1`"
 		   done
- >&2 echo "XXXXX 6"
-
 			if ( [ "${gitfetchno}" = "5" ] )
 			then
 				status "Had trouble getting the webserver infrastructure sourcecode, will have to exit"
@@ -236,7 +228,7 @@ do
 			status "To access this machine once it has finished provisioning you can use the scripts in ${BUILD_HOME}/helperscripts"
 			status "Log files (stderr and stdout) are stored on the remote machine in the directory /home/${SERVER_USER}/logs"
 			status "Starting to build the webserver proper"
-			/bin/date >&3
+			status "`/bin/date`"
 
 		   #Which one is a called depends on what we are building from. Virgin, hourly, weekly, monthly or bimonthly
 			if ( [ "${BUILD_CHOICE}" = "0" ] )
