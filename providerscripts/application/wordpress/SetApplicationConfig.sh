a5694d762ebe6299295e5a8df1e3ca2b6c3070de
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-21 11:19:29 +0100

    Update SetApplicationConfig.sh

--- providerscripts/application/wordpress/SetApplicationConfig.sh
+++ providerscripts/application/wordpress/SetApplicationConfig.sh
@@ -1,7 +1,7 @@
 
 dbprefix="`${BUILD_HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh ${WEBSITE_URL} DBPREFIX:* | /usr/bin/awk -F':' '{print $NF}'`"
 
-/bin/sed -i "/DB_HOST/c\ define('DB_HOST', \"${db_identifier}:${DB_PORT}\");" ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
+/bin/sed -i "/DB_HOST/c\ define('DB_HOST', \"${database_identifier}:${DB_PORT}\");" ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
 /bin/sed -i "/DB_USER/c\ define('DB_USER', \"${database_username}\");" ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
 /bin/sed -i "/DB_PASSWORD/c\ define('DB_PASSWORD', \"${database_password}\");" ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
 /bin/sed -i "/DB_NAME/c\ define('DB_NAME', \"${database_name}\");" ${BUILD_HOME}/buildconfiguration/wp-config-sample.php
