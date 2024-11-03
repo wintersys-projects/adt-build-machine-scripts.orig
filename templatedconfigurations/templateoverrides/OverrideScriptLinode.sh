4b26664b30ea461a53e5b5455b38088f0818f61e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-03 19:38:08 +0000

    Update OverrideScriptLinode.sh

--- templatedconfigurations/templateoverrides/OverrideScriptLinode.sh
+++ templatedconfigurations/templateoverrides/OverrideScriptLinode.sh
@@ -73,6 +73,9 @@ firewall=""
 if ( [ "`/bin/grep "^FIREWALL:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "ufw" ] )
 then
 	firewall="ufw"
+elif ( [ "`/bin/grep "^FIREWALL:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "iptables" ] )
+then
+	firewall="iptables"
 fi
 
 if ( [ "${firewall}" = "ufw" ] )
