b22d7620ed3fa4f2089ee778a0e4b084450c59ce
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-03 20:35:23 +0000

    Update InitialiseFirewall.sh

--- providerscripts/security/firewall/InitialiseFirewall.sh
+++ providerscripts/security/firewall/InitialiseFirewall.sh
@@ -21,6 +21,8 @@
 #######################################################################################################
 #set -x
 
+BUILD_HOME="`/usr/bin/pwd`"
+
 firewall=""
 if ( [ "`/bin/grep "^FIREWALL:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "ufw" ] )
 then
@@ -30,31 +32,34 @@ then
 	firewall="iptables"
 fi
 
+laptop_ip="`/bin/ls ${BUILD_HOME}/runtimedata/LAPTOPIP:* | /usr/bin/awk -F':' '{print $NF}'`"
+buildmachine_ssh_port="`/bin/ls ${BUILD_HOME}/runtimedata/BUILDMACHINEPORT:* | /usr/bin/awk -F':' '{print $NF}'`"
+
 if ( [ "${firewall}" = "ufw" ] )
 then
-	/usr/bin/apt-get -qq -y install ufw
+	#/usr/bin/apt-get -qq -y install ufw
 	/bin/echo "y" | /usr/sbin/ufw reset	
 	/usr/sbin/ufw default deny incoming
 	/usr/sbin/ufw default allow outgoing
 	#uncomment this if you want more general access than just ssh
 	#/usr/sbin/ufw allow from ${LAPTOP_IP}
-	/usr/sbin/ufw allow from ${LAPTOP_IP} to any port ${BUILDMACHINE_SSH_PORT}
+	/usr/sbin/ufw allow from ${laptop_ip} to any port ${buildmachine_ssh_port}
 	/bin/echo "y" | /usr/sbin/ufw enable
 elif ( [ "${firewall}" = "iptables" ] )
 then
-        /usr/bin/apt-get -qq -y install iptables
+       # /usr/bin/apt-get -qq -y install iptables
 
-        /usr/bin/debconf-set-selections <<EOF
-iptables-persistent iptables-persistent/autosave_v4 boolean true
-iptables-persistent iptables-persistent/autosave_v6 boolean true
-EOF
-        /usr/bin/apt install -y -qq netfilter-persistent
+      #  /usr/bin/debconf-set-selections <<EOF
+#iptables-persistent iptables-persistent/autosave_v4 boolean true
+#iptables-persistent iptables-persistent/autosave_v6 boolean true
+#EOF
+ #       /usr/bin/apt install -y -qq netfilter-persistent
         /usr/sbin/iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-        /usr/sbin/iptables -A INPUT -p tcp --dport ${BUILDMACHINE_SSH_PORT} -j ACCEPT
-        /usr/sbin/iptables -A INPUT -s ${LAPTOP_IP} -p ICMP --icmp-type 8 -j ACCEPT
+        /usr/sbin/iptables -A INPUT -p tcp --dport ${buildmachine_ssh_port} -j ACCEPT
+        /usr/sbin/iptables -A INPUT -s ${laptop_ip} -p ICMP --icmp-type 8 -j ACCEPT
         /usr/sbin/iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j DROP
-        /usr/sbin/iptables -I INPUT \! -s ${LAPTOP_IP} -m state --state NEW,INVALID -p tcp --dport ${BUILDMACHINE_SSH_PORT} -j DROP
-        /usr/sbin/iptables -A INPUT ! -s ${LAPTOP_IP} -p icmp -m state --state INVALID,NEW -m icmp --icmp-type 8  -j DROP
+        /usr/sbin/iptables -I INPUT \! -s ${laptop_ip} -m state --state NEW,INVALID -p tcp --dport ${buildmachine_ssh_port} -j DROP
+        /usr/sbin/iptables -A INPUT ! -s ${laptop_ip} -p icmp -m state --state INVALID,NEW -m icmp --icmp-type 8  -j DROP
         /usr/sbin/iptables -A INPUT -i lo -j ACCEPT
         /usr/sbin/iptables -A OUTPUT -o lo -j ACCEPT
         /usr/sbin/iptables -P INPUT DROP
