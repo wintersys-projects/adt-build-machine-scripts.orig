c4db3d9ca13d2f7fc71bc7107a71aa64fb1b7b82
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-07 12:02:58 +0000

    Update DeleteFromDatastore.sh

--- providerscripts/datastore/DeleteFromDatastore.sh
+++ providerscripts/datastore/DeleteFromDatastore.sh
@@ -26,10 +26,12 @@ BUILD_HOME="$3"
 
 if ( [ "${BUILD_HOME}" = "" ] )
 then 
-	BUILD_HOME="`/usr/bin/pwd | /bin/sed 's/\/helperscripts//g'`"
+        BUILD_HOME="`/usr/bin/pwd | /bin/sed 's/\/helperscripts//g'`"
 fi
 
 if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "s3cmd" ] )
 then
-	/usr/bin/s3cmd del --force --recursive s3://${file_to_delete}
+        datastore_tool="/usr/bin/s3cmd"
 fi
+
+${datastore_tool} del --force --recursive s3://${file_to_delete}
