0afb6e3cbba64f06c5377ac51e89ca0ddb324c9d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-07 17:42:05 +0000

    Update DeleteFromDatastore.sh

--- providerscripts/datastore/DeleteFromDatastore.sh
+++ providerscripts/datastore/DeleteFromDatastore.sh
@@ -31,11 +31,11 @@ fi
 
 if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "s3cmd" ] )
 then
-        datastore_tool="/usr/bin/s3cmd"
+        datastore_tool="/usr/bin/s3cmd --force"
 elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "s5cmd" ] )
 then
         host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
         datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} "
 fi
 
-${datastore_tool} del --force --recursive s3://${file_to_delete}
+${datastore_tool} del --recursive s3://${file_to_delete}
