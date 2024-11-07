2833277e5f67d3dd30c5bf88bfecad31b6e0b44b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-07 17:09:39 +0000

    Update SyncDatastore.sh

--- providerscripts/datastore/SyncDatastore.sh
+++ providerscripts/datastore/SyncDatastore.sh
@@ -29,6 +29,10 @@ new_object="$3"
 if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "s3cmd" ] )
 then
         datastore_tool="/usr/bin/s3cmd"
+elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "s5cmd" ] )
+then
+        host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} "
 fi
 
 ${datastore_tool} sync s3://${original_object} s3://${new_object} 2>/dev/null
