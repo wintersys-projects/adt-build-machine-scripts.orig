571f52d9e1e90d17b9fe7da2db8093c05275df15
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-12 15:48:04 +0000

    Update ListServerIDs.sh

--- providerscripts/server/ListServerIDs.sh
+++ providerscripts/server/ListServerIDs.sh
@@ -42,8 +42,7 @@ fi
 if ( [ "${cloudhost}" = "vultr" ] )
 then
 	export VULTR_API_KEY="`/bin/cat ${BUILD_HOME}/runtimedata/${cloudhost}/TOKEN`"
-	/bin/sleep 1
- 	#server_type="`/bin/echo ${server_type} | /bin/sed 's/\*//g'`"
+	server_type="`/bin/echo ${server_type} | /usr/bin/cut -c -25`"
  	/usr/bin/vultr instance list -o json | /usr/bin/jq '.instances[] | select (.label | contains("'${server_type}'")).id' | /bin/sed 's/"//g'
 fi
 
