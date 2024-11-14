209445398c1f5681df3a4e43c00cad9c81c5a7d2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-14 14:31:33 +0000

    Create CheckBuildMachineVPC.sh

---
+++ providerscripts/server/CheckBuildMachineVPC.sh
@@ -0,0 +1,15 @@
+
+
+
+cloudhost="${1}"
+server_name="${2}"
+
+if ( [ "${cloudhost}" = "vultr" ] )
+then
+  if ( [ "`/usr/bin/vultr instance list -o json | /usr/bin/jq -r '.instances[] | select (.label == "'${server_name}'").id'`" = "" ] )
+  then
+    status "It looks like the build machine (${server_name}) is not attached to a VPC when BUILD_MACHINE_VPC=1"
+    status "Will have to exit (change BUILD_MACHINE_VPC if necessary)"
+    exit
+  fi
+fi
