1e2d4b73082315cf553a98629a1f9c0fa2b02587
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-14 15:51:18 +0000

    Update CheckBuildMachineVPC.sh

--- providerscripts/server/CheckBuildMachineVPC.sh
+++ providerscripts/server/CheckBuildMachineVPC.sh
@@ -1,5 +1,7 @@
 
 
+
+
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 
 cloudhost="${1}"
@@ -8,13 +10,5 @@ server_ip="${2}"
 if ( [ "${cloudhost}" = "vultr" ] )
 then
         server_name="`${BUILD_HOME}/providerscripts/server/GetServerName.sh ${server_ip} vultr`"
-        private_ip="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh ${server_name} vultr`"
-        vpc_id="`/usr/bin/vultr vpc2 list -o json | /usr/bin/jq -r '.vpcs[] | select (.description == "adt-vpc").id'`"
-        checked_private_ip="`/usr/bin/vultr vpc2 nodes list ${vpc_id} -o json | /usr/bin/jq -r '.nodes[] | select (.description == "'${server_name}'").ip_address'`"
-
-        if ( [ "${private_ip}" != "${checked_private_ip}" ] )
-        then
-                status "It looks like the build machine (${server_name}) is not attached to a VPC when BUILD_MACHINE_VPC=1"
-                status "Will have to exit (change BUILD_MACHINE_VPC if necessary)"
-        fi
+        ${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh ${server_name} vultr 
 fi
