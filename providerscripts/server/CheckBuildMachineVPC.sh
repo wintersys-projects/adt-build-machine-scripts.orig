03fd1dd6dcf64edde374a37f335303d67194a6a5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-14 16:18:38 +0000

    Update CheckBuildMachineVPC.sh

--- providerscripts/server/CheckBuildMachineVPC.sh
+++ providerscripts/server/CheckBuildMachineVPC.sh
@@ -7,6 +7,24 @@ BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 cloudhost="${1}"
 server_ip="${2}"
 
+if ( [ "${cloudhost}" = "digitalocean" ] )
+then
+        server_name="`${BUILD_HOME}/providerscripts/server/GetServerName.sh ${server_ip} vultr`"
+        ${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh ${server_name} vultr 
+fi
+
+if ( [ "${cloudhost}" = "exoscale" ] )
+then
+        server_name="`${BUILD_HOME}/providerscripts/server/GetServerName.sh ${server_ip} vultr`"
+        ${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh ${server_name} vultr 
+fi
+
+if ( [ "${cloudhost}" = "linode" ] )
+then
+        server_name="`${BUILD_HOME}/providerscripts/server/GetServerName.sh ${server_ip} vultr`"
+        ${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh ${server_name} vultr 
+fi
+
 if ( [ "${cloudhost}" = "vultr" ] )
 then
         server_name="`${BUILD_HOME}/providerscripts/server/GetServerName.sh ${server_ip} vultr`"
