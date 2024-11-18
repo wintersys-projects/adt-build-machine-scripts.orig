f757f46bf55f73d09ea9f16a2c90a913dae50605
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-18 02:47:18 +0000

    Update OverrideScriptLinode.sh

--- templatedconfigurations/templateoverrides/OverrideScriptLinode.sh
+++ templatedconfigurations/templateoverrides/OverrideScriptLinode.sh
@@ -22,25 +22,36 @@
 
 #XXXSTACKYYY
 
+set -x
+
+OUT_FILE="buildmachine-out-`/bin/date | /bin/sed 's/ //g'`"
+exec 1>>/root/${OUT_FILE}
+ERR_FILE="buildmachine-err-`/bin/date | /bin/sed 's/ //g'`"
+exec 2>>/root/${ERR_FILE}
+
 /usr/sbin/adduser --disabled-password --gecos \"\" ${BUILDMACHINE_USER} 
+/bin/sed -i '$ a\ ClientAliveInterval 60\nTCPKeepAlive yes\nClientAliveCountMax 10000' /etc/ssh/sshd_config
 /bin/echo ${BUILDMACHINE_USER}:${BUILDMACHINE_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/chpasswd 
-/usr/bin/gpasswd -a ${BUILDMACHINE_USER} sudo 
+ /usr/bin/gpasswd -a ${BUILDMACHINE_USER} sudo 
+
 /bin/mkdir -p /home/${BUILDMACHINE_USER}/.ssh
 /bin/echo "${SSH}" >> /home/${BUILDMACHINE_USER}/.ssh/authorized_keys
 
-/bin/sed -i '$ a\ ClientAliveInterval 60\nTCPKeepAlive yes\nClientAliveCountMax 10000' /etc/ssh/sshd_config
-/bin/sed -i 's/PasswordAuthentication.*$/PasswordAuthentication no/' /etc/ssh/sshd_config
-/bin/sed -i 's/^#PasswordAuthentication.*$/PasswordAuthentication no/' /etc/ssh/sshd_config
-/bin/sed -i "s/^PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/sshd_config
-/bin/sed -i "s/^#PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/sshd_config
-/bin/sed -i "s/^KbdInteractiveAuthentication.*/KbdInteractiveAuthentication no/g" /etc/ssh/sshd_config
-/bin/sed -i "s/^#KbdInteractiveAuthentication.*/KbdInteractiveAuthentication no/g" /etc/ssh/sshd_config 	
-/bin/sed -i "s/^ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/g" /etc/ssh/sshd_config
-/bin/sed -i "s/^#ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/g" /etc/ssh/sshd_config
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^PasswordAuthentication.*/PasswordAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^PermitRootLogin.*/PermitRootLogin no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^#PermitRootLogin.*/PermitRootLogin no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^KbdInteractiveAuthentication.*/KbdInteractiveAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^#KbdInteractiveAuthentication.*/KbdInteractiveAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^#ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^AddressFamily.*/AddressFamily inet/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^#AddressFamily.*/AddressFamily inet/g' {} +
+
 
 if ( [ "${BUILDMACHINE_SSH_PORT}" = "" ] )
 then
-	BUILDMACHINE_SSH_PORT="22"
+        BUILDMACHINE_SSH_PORT="22"
 fi
 
 /bin/sed -i "s/^Port.*$/Port ${BUILDMACHINE_SSH_PORT}/g" /etc/ssh/sshd_config
@@ -49,14 +60,13 @@ fi
 /usr/bin/apt-get -qq -y update
 /usr/bin/apt-get -qq -y install git
 
-#/usr/sbin/ufw allow from ${LAPTOP_IP} to any port ${BUILDMACHINE_SSH_PORT}
 cd /home/${BUILDMACHINE_USER}
 
 if ( [ "${INFRASTRUCTURE_REPOSITORY_OWNER}" != "" ] )
 then
-	/usr/bin/git clone https://github.com/${INFRASTRUCTURE_REPOSITORY_OWNER}/adt-build-machine-scripts.git
+        /usr/bin/git clone https://github.com/${INFRASTRUCTURE_REPOSITORY_OWNER}/adt-build-machine-scripts.git
 else
-	/usr/bin/git clone https://github.com/wintersys-projects/adt-build-machine-scripts.git
+        /usr/bin/git clone https://github.com/wintersys-projects/adt-build-machine-scripts.git
 fi
 
 /bin/mkdir -p /home/${BUILDMACHINE_USER}/adt-build-machine-scripts/logs
@@ -72,16 +82,13 @@ exec 2>>/home/${BUILDMACHINE_USER}/adt-build-machine-scripts/logs/${ERR_FILE}
 BUILD_HOME="/home/${BUILDMACHINE_USER}/adt-build-machine-scripts"
 /bin/echo ${BUILD_HOME} > /home/buildhome.dat
 /bin/sh ${BUILD_HOME}/helperscripts/RunServiceCommand.sh ssh restart
-
+ 
 if ( [ ! -d ${BUILD_HOME}/runtimedata ] )
 then
-	/bin/mkdir -p ${BUILD_HOME}/runtimedata
+        /bin/mkdir -p ${BUILD_HOME}/runtimedata
 fi
 /bin/touch ${BUILD_HOME}/runtimedata/LAPTOPIP:${LAPTOP_IP}
 /bin/touch ${BUILD_HOME}/runtimedata/BUILDMACHINEPORT:${BUILDMACHINE_SSH_PORT}
 
 /bin/sh ${BUILD_HOME}/installscripts/InstallFirewall.sh "`/bin/grep ^ID /etc/*-release | /usr/bin/awk -F'=' '{print $NF}'`"
 . ${BUILD_HOME}/providerscripts/security/firewall/InitialiseFirewall.sh 
-
-
-
