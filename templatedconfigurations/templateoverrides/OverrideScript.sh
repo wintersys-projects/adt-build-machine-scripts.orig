335d34df6efabce802652833e953ff59603e4a4b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-18 02:46:18 +0000

    Update OverrideScript.sh

--- templatedconfigurations/templateoverrides/OverrideScript.sh
+++ templatedconfigurations/templateoverrides/OverrideScript.sh
@@ -42,6 +42,13 @@ export SELECTED_TEMPLATE=\"\" #set if using hardcore build
 
 #XXXSTACKYYY
 
+set -x
+
+OUT_FILE="buildmachine-out-`/bin/date | /bin/sed 's/ //g'`"
+exec 1>>/root/${OUT_FILE}
+ERR_FILE="buildmachine-err-`/bin/date | /bin/sed 's/ //g'`"
+exec 2>>/root/${ERR_FILE}
+
 /usr/sbin/adduser --disabled-password --gecos \"\" ${BUILDMACHINE_USER} 
 /bin/sed -i '$ a\ ClientAliveInterval 60\nTCPKeepAlive yes\nClientAliveCountMax 10000' /etc/ssh/sshd_config
 /bin/echo ${BUILDMACHINE_USER}:${BUILDMACHINE_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/chpasswd 
@@ -50,35 +57,21 @@ export SELECTED_TEMPLATE=\"\" #set if using hardcore build
 /bin/mkdir -p /home/${BUILDMACHINE_USER}/.ssh
 /bin/echo "${SSH}" >> /home/${BUILDMACHINE_USER}/.ssh/authorized_keys
 
-#if ( [ -f /etc/systemd/system/ssh.service.d/00-socket.conf ] )
-#then
-#	/bin/rm /etc/systemd/system/ssh.service.d/00-socket.conf
-#	/bin/systemctl daemon-restart
-#fi
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^PasswordAuthentication.*/PasswordAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^PermitRootLogin.*/PermitRootLogin no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^#PermitRootLogin.*/PermitRootLogin no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^KbdInteractiveAuthentication.*/KbdInteractiveAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^#KbdInteractiveAuthentication.*/KbdInteractiveAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^#ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^AddressFamily.*/AddressFamily inet/g' {} +
+/usr/bin/find /etc/ssh -name '*' -type f -exec sed -i 's/^#AddressFamily.*/AddressFamily inet/g' {} +
 
-#/bin/systemctl disable --now ssh.socket
-#/bin/systemctl enable --now ssh.service
-
-/bin/sed -i 's/#*PasswordAuthentication [a-zA-Z]*/PasswordAuthentication no/' /etc/ssh/sshd_config
-
-for file in `/bin/ls /etc/ssh/sshd_config.d`
-do
-	fullfile="/etc/ssh/sshd_config.d/${file}"
-	/bin/sed -i 's/PasswordAuthentication.*$/PasswordAuthentication no/' ${fullfile}
- 	/bin/sed -i 's/^#PasswordAuthentication.*$/PasswordAuthentication no/' ${fullfile}
- 	/bin/sed -i "s/^PermitRootLogin.*/PermitRootLogin no/g" ${fullfile}
-	/bin/sed -i "s/^#PermitRootLogin.*/PermitRootLogin no/g" ${fullfile}
- 	/bin/sed -i "s/^KbdInteractiveAuthentication.*/KbdInteractiveAuthentication no/g" ${fullfile}
-	/bin/sed -i "s/^#KbdInteractiveAuthentication.*/KbdInteractiveAuthentication no/g" ${fullfile} 	
- 	/bin/sed -i "s/^ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/g" ${fullfile}
-	/bin/sed -i "s/^#ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/g" ${fullfile}
-  	/bin/sed -i "s/^AddressFamily.*/AddressFamily inet/g" ${fullfile}
-	/bin/sed -i "s/^#AddressFamily.*/AddressFamily inet/g" ${fullfile}
-done
 
 if ( [ "${BUILDMACHINE_SSH_PORT}" = "" ] )
 then
-	BUILDMACHINE_SSH_PORT="22"
+        BUILDMACHINE_SSH_PORT="22"
 fi
 
 /bin/sed -i "s/^Port.*$/Port ${BUILDMACHINE_SSH_PORT}/g" /etc/ssh/sshd_config
@@ -91,9 +84,9 @@ cd /home/${BUILDMACHINE_USER}
 
 if ( [ "${INFRASTRUCTURE_REPOSITORY_OWNER}" != "" ] )
 then
-	/usr/bin/git clone https://github.com/${INFRASTRUCTURE_REPOSITORY_OWNER}/adt-build-machine-scripts.git
+        /usr/bin/git clone https://github.com/${INFRASTRUCTURE_REPOSITORY_OWNER}/adt-build-machine-scripts.git
 else
-	/usr/bin/git clone https://github.com/wintersys-projects/adt-build-machine-scripts.git
+        /usr/bin/git clone https://github.com/wintersys-projects/adt-build-machine-scripts.git
 fi
 
 /bin/mkdir -p /home/${BUILDMACHINE_USER}/adt-build-machine-scripts/logs
@@ -112,7 +105,7 @@ BUILD_HOME="/home/${BUILDMACHINE_USER}/adt-build-machine-scripts"
  
 if ( [ ! -d ${BUILD_HOME}/runtimedata ] )
 then
-	/bin/mkdir -p ${BUILD_HOME}/runtimedata
+        /bin/mkdir -p ${BUILD_HOME}/runtimedata
 fi
 /bin/touch ${BUILD_HOME}/runtimedata/LAPTOPIP:${LAPTOP_IP}
 /bin/touch ${BUILD_HOME}/runtimedata/BUILDMACHINEPORT:${BUILDMACHINE_SSH_PORT}
