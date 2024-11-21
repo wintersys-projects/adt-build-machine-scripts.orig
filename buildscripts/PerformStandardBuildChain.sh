2077110aefcbe667594d40af7c3ce701e5955de8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-21 14:37:06 +0000

    Update PerformStandardBuildChain.sh

--- buildscripts/PerformStandardBuildChain.sh
+++ buildscripts/PerformStandardBuildChain.sh
@@ -25,37 +25,10 @@ then
         /bin/rm ${BUILD_HOME}/keys/${CLOUDHOST}/${BUILD_IDENTIFIER}/autoscaler_keys
 fi
 
-if ( [ "${AUTOSCALE_FROM_SNAPSHOTS}" = "1" ] )
-then
-        status "###############################################################################################################################################"
-        status "Performing a snapshot style build. If this is what you want, press <enter> if not <ctrl-c> to reconfigure"
-        status "###############################################################################################################################################"
-        if ( [ "${HARDCORE}" != "1" ] )
-        then
-                read carryon
-        fi
+pids=""
 
-        if ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS" ] )
-        then
-                status "##############################################################################################################"
-                status "You have selected to use a 3rd party DBaaS for your database solution"
-                status "It is fine to do this when building from snapshots, but there is a caveat that all the credentials have to be"
-                status "the same as when you generated the snapshot as part of an initial build process"
-                status "This means that you can't have changed the username, password, hostname, ip addresses and so on in the interim"
-                status "period between when you generated the snapshot you are now building from and now."
-                status "I am expecting you to have the following active and online"
-                status "A Database with username: ${DBaaS_USERNAME}"
-                status "A Database with password: ${DBaaS_PASSWORD}"
-                status "A Database with the name: ${DBaaS_DBNAME}"
-                status "A Database at Endpoint: ${DBaaS_HOSTNAME}"
-                status "##############################################################################################################"
-                status "If all these things are configured and set, then it is OK to :"
-                status "Press the <enter> key to continue"
-                if ( [ "${HARDCORE}" != "1" ] )
-                then
-                        read x
-                fi
-        fi
+if ( [ "${PRODUCTION}" = "1" ] && [ "${DEVELOPMENT}" = "0" ] && [ "${BASELINE_DB_REPOSITORY}" != "VIRGIN" ] )
+then
         if ( [ "${NO_AUTOSCALERS}" = "" ] )
         then
                 status "How many autoscalers do you want to deploy?"
@@ -63,8 +36,8 @@ then
                 if ( [ "${HARDCORE}" != "1" ] )
                 then
                         read NO_AUTOSCALERS
-                fi
-                 
+                fi 
+
                 while ! ( [ "${NO_AUTOSCALERS}" -eq "${NO_AUTOSCALERS}" ] 2> /dev/null )
                 do
                         status "Sorry, invalid input, try again"
@@ -92,7 +65,7 @@ then
                         fi
                         NO_AUTOSCALERS="5"
                 else 
-                        status "Number of autoscalers is set to ${NO_AUTOSCALERS} are you sure?"
+                        status "Number of autoscalers set to ${NO_AUTOSCALERS}"
                         status "Press <enter> to accept"
                         if ( [ "${HARDCORE}" != "1" ] )
                         then
@@ -100,180 +73,118 @@ then
                         fi
                 fi
         fi
-
-        . ${BUILD_HOME}/buildscripts/BuildFromSnapshots.sh
-        . ${BUILD_HOME}/providerscripts/security/firewall/TightenDBaaSFirewall.sh
-        /bin/touch ${BUILD_HOME}/runtimedata/PRIME_FIREWALL
-        cloudhost_holder="${CLOUDHOST}"
-        . ${BUILD_HOME}/providerscripts/security/firewall/TightenBuildMachineFirewall.sh
-        export CLOUDHOST="${cloudhost_holder}"
-
-        status ""
-        status "##########################################################################################################"
-        status "Build from snapshots completed"
-        status "It will take a few minutes before your site comes online. Anything more than 10 minutes before it is online"
-        status "Means that there is something wrong and you should start to investigate"
-        status "##########################################################################################################"
-else
-
-        if ( [ "${PRODUCTION}" = "1" ] && [ "${DEVELOPMENT}" = "0" ] && [ "${BASELINE_DB_REPOSITORY}" != "VIRGIN" ] )
+        if ( [ "${NO_AUTOSCALERS}" -ne "0" ] && [ "${INPARALLEL}" = "0" ] )
         then
-                        if ( [ "${NO_AUTOSCALERS}" = "" ] )
-                        then
-                                status "How many autoscalers do you want to deploy?"
-                                if ( [ "${HARDCORE}" != "1" ] )
-                                then
-                                        read NO_AUTOSCALERS
-                                fi 
-                                while ! ( [ "${NO_AUTOSCALERS}" -eq "${NO_AUTOSCALERS}" ] 2> /dev/null )
-                                do
-                                        status "Sorry, invalid input, try again"
-                                        if ( [ "${HARDCORE}" != "1" ] )
-                                        then
-                                                read NO_AUTOSCALERS
-                                        fi
-                                done
-                                if ( [ "${NO_AUTOSCALERS}" -lt "1" ] )
-                                then
-                                        status "Number of autoscalers can't be less than 1 setting autoscalers to 1"
-                                        status "Press <enter> to accept"
-                                        if ( [ "${HARDCORE}" != "1" ] )
-                                        then
-                                                read x
-                                        fi
-                                        NO_AUTOSCALERS="1"
-                                elif ( [ "${NO_AUTOSCALERS}" -gt "5" ] )
-                                then
-                                        status "Number of autoscalers can't be greater than 5 setting autoscalers to 5"
-                                        status "Press <enter> to accept"
-                                        if ( [ "${HARDCORE}" != "1" ] )
-                                        then
-                                                read x
-                                        fi
-                                        NO_AUTOSCALERS="5"
-                                else 
-                                        status "Number of autoscalers set to ${NO_AUTOSCALERS}"
-                                        status "Press <enter> to accept"
-                                        if ( [ "${HARDCORE}" != "1" ] )
-                                        then
-                                                read x
-                                        fi
-                                fi
-                        fi
-
-                if ( [ "${NO_AUTOSCALERS}" -ne "0" ] && [ "${INPARALLEL}" = "0" ] )
+                tally="0"
+                if ( [ -f ${BUILD_HOME}/runtimedata/MULTI_AUTOSCALER_BUILT ] )
                 then
-                        tally="0"
-                        if ( [ -f ${BUILD_HOME}/runtimedata/MULTI_AUTOSCALER_BUILT ] )
-                        then
-                                /bin/rm ${BUILD_HOME}/runtimedata/MULTI_AUTOSCALER_BUILT
-                        fi
-                        while ( [ "${tally}" -lt "${NO_AUTOSCALERS}" ] && [ ! -f ${BUILD_HOME}/runtimedata/MULTI_AUTOSCALER_BUILT ] )
-                        do
-                                . ${BUILD_HOME}/buildscripts/BuildAutoscaler.sh
-                                tally="`/usr/bin/expr ${tally} + 1`"
-                        done
-                elif ( [ "${NO_AUTOSCALERS}" -ne "0" ] && [ "${INPARALLEL}" = "1" ] )
-                then
-                        tally="0"
-                        while ( [ "${NO_AUTOSCALERS}" -le "5" ] && [ "${tally}" -lt "${NO_AUTOSCALERS}" ] )
-                        do
-                                . ${BUILD_HOME}/buildscripts/BuildAutoscaler.sh &
-                                pids="${pids} $!"
-                                /bin/sleep 30
-                                tally="`/usr/bin/expr ${tally} + 1`"
-                        done
+                        /bin/rm ${BUILD_HOME}/runtimedata/MULTI_AUTOSCALER_BUILT
                 fi
-        fi
-
-        if ( [ "${INPARALLEL}" = "0" ] )
-        then
-                . ${BUILD_HOME}/buildscripts/BuildWebserver.sh 
-                . ${BUILD_HOME}/buildscripts/BuildDatabase.sh 
-        elif ( [ "${NO_AUTOSCALERS}" -ne "0" ] && [ "${INPARALLEL}" = "1" ]  )
-        then
-                . ${BUILD_HOME}/buildscripts/BuildWebserver.sh &
-                pids="${pids} $!"
-
-                /bin/sleep 30
-                . ${BUILD_HOME}/buildscripts/BuildDatabase.sh &
-                pids="${pids} $!"
-
-        fi
-
-        if ( [ "${NO_AUTOSCALERS}" -eq "0" ] && [ "${INPARALLEL}" = "1" ]  && [ "${DEVELOPMENT}" = "1" ] )
-        then
-                . ${BUILD_HOME}/buildscripts/BuildWebserver.sh &
-                pids="${pids} $!"
-                /bin/sleep 30
-                . ${BUILD_HOME}/buildscripts/BuildDatabase.sh &
-                pids="${pids} $!"
-        fi
-
-        for pid in ${pids}
-        do
-                wait ${pid}
-        done
-
-        if ( [ "${BUILD_MACHINE_VPC}" = "0" ] )
-        then
-                AUTOSCALER_PUBLIC_KEYS="${BUILD_HOME}/keys/${CLOUDHOST}/${BUILD_IDENTIFIER}/autoscaler_keys"
-                if ( [ "${PRODUCTION}" = "1" ] && [ "${NO_AUTOSCALERS}" -gt "1" ] )
-                then
-                        as_active_ips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "autoscaler" "${CLOUDHOST}"`"
-                elif ( [ "${PRODUCTION}" = "1" ] && [ "${NO_AUTOSCALERS}" -eq "1" ] )
-                then
-                        as_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "autoscaler" "${CLOUDHOST}"`"
-                fi
-                ws_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "webserver" "${CLOUDHOST}"`"
-                db_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "database" "${CLOUDHOST}"`"
-        elif ( [ "${BUILD_MACHINE_VPC}" = "1" ] )
-        then
-                if ( [ "${PRODUCTION}" = "1" ] && [ "${NO_AUTOSCALERS}" -gt "1" ] )
-                then
-                        as_active_ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "autoscaler" "${CLOUDHOST}"`"
-                elif ( [ "${PRODUCTION}" = "1" ] && [ "${NO_AUTOSCALERS}" -eq "1" ] )
-                then
-                        as_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "autoscaler" "${CLOUDHOST}"`"
-                fi
-                ws_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "webserver" "${CLOUDHOST}"`"
-                db_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "database" "${CLOUDHOST}"`"
-        fi
-
-        AUTOSCALER_PUBLIC_KEYS="${BUILD_HOME}/keys/${CLOUDHOST}/${BUILD_IDENTIFIER}/autoscaler_keys"
-        OPTIONS="-o ConnectTimeout=10 -o ConnectionAttempts=5 -o UserKnownHostsFile=${AUTOSCALER_PUBLIC_KEYS} -o StrictHostKeyChecking=yes "
-        if ( [ "${as_active_ips}" != "" ] )
-        then
-                for ip in `/bin/echo ${as_active_ips} | /bin/sed 's/:/ /g'`
+                while ( [ "${tally}" -lt "${NO_AUTOSCALERS}" ] && [ ! -f ${BUILD_HOME}/runtimedata/MULTI_AUTOSCALER_BUILT ] )
                 do
-                        /usr/bin/scp ${OPTIONS} -P ${SSH_PORT} ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/webserver_configuration_settings.dat ${SERVER_USER}@${ip}:/home/${SERVER_USER}/.ssh/webserver_configuration_settings.dat >/dev/null 2>&1        
-                        /usr/bin/scp ${OPTIONS} -P ${SSH_PORT} ${BUILD_HOME}/builddescriptors/buildstylesscp.dat ${SERVER_USER}@${ip}:/home/${SERVER_USER}/.ssh/buildstyles.dat >/dev/null 2>&1    
+                        . ${BUILD_HOME}/buildscripts/BuildAutoscaler.sh
+                        tally="`/usr/bin/expr ${tally} + 1`"
                 done
-        elif ( [ "${as_active_ip}" != "" ] )
+        elif ( [ "${NO_AUTOSCALERS}" -ne "0" ] && [ "${INPARALLEL}" = "1" ] )
         then
-                /usr/bin/scp ${OPTIONS} -P ${SSH_PORT} ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/webserver_configuration_settings.dat ${SERVER_USER}@${as_active_ip}:/home/${SERVER_USER}/.ssh/webserver_configuration_settings.dat >/dev/null 2>&1        
-                /usr/bin/scp ${OPTIONS} -P ${SSH_PORT} ${BUILD_HOME}/builddescriptors/buildstylesscp.dat ${SERVER_USER}@${as_active_ip}:/home/${SERVER_USER}/.ssh/buildstyles.dat >/dev/null 2>&1    
+                tally="0"
+                while ( [ "${NO_AUTOSCALERS}" -le "5" ] && [ "${tally}" -lt "${NO_AUTOSCALERS}" ] )
+                do
+                        . ${BUILD_HOME}/buildscripts/BuildAutoscaler.sh &
+                        pids="${pids} $!"
+                        /bin/sleep 30
+                        tally="`/usr/bin/expr ${tally} + 1`"
+                done
         fi
+fi 
 
-        if ( [ "${PRODUCTION}" = "1" ] )
-        then
-                status "Autoscaler, webserver and database built correctly....."
-        elif ( [ "${DEVELOPMENT}" = "1" ] )
-        then
-                status "Webserver and database built correctly....."
-        fi
+if ( [ "${INPARALLEL}" = "0" ] )
+then
+        . ${BUILD_HOME}/buildscripts/BuildWebserver.sh 
+        . ${BUILD_HOME}/buildscripts/BuildDatabase.sh 
+elif ( [ "${NO_AUTOSCALERS}" -ne "0" ] && [ "${INPARALLEL}" = "1" ]  )
+then
+        . ${BUILD_HOME}/buildscripts/BuildWebserver.sh &
+        pids="${pids} $!"
 
-        . ${BUILD_HOME}/providerscripts/security/firewall/TightenDBaaSFirewall.sh
-        /bin/touch ${BUILD_HOME}/runtimedata//PRIME_FIREWALL
-        cloudhost_holder="${CLOUDHOST}"
-        . ${BUILD_HOME}/providerscripts/security/firewall/TightenBuildMachineFirewall.sh
-        export CLOUDHOST="${cloudhost_holder}"
-
-        if ( [ "${BUILD_ARCHIVE_CHOICE}" != "virgin" ] )
-        then
-                . ${BUILD_HOME}/providerscripts/application/SetApplicationConfig.sh
-        fi
-       
-        ##Do the build finalisation procedures
-        . ${BUILD_HOME}/buildscripts/FinaliseBuildProcessing.sh
+        /bin/sleep 30
+        . ${BUILD_HOME}/buildscripts/BuildDatabase.sh &
+        pids="${pids} $!"
 fi
+
+if ( [ "${NO_AUTOSCALERS}" -eq "0" ] && [ "${INPARALLEL}" = "1" ]  && [ "${DEVELOPMENT}" = "1" ] )
+then
+        . ${BUILD_HOME}/buildscripts/BuildWebserver.sh &
+        pids="${pids} $!"
+        /bin/sleep 30
+        . ${BUILD_HOME}/buildscripts/BuildDatabase.sh &
+        pids="${pids} $!"
+fi
+
+for pid in ${pids}
+do
+        wait ${pid}
+done
+
+if ( [ "${BUILD_MACHINE_VPC}" = "0" ] )
+then
+        AUTOSCALER_PUBLIC_KEYS="${BUILD_HOME}/keys/${CLOUDHOST}/${BUILD_IDENTIFIER}/autoscaler_keys"
+        if ( [ "${PRODUCTION}" = "1" ] && [ "${NO_AUTOSCALERS}" -gt "1" ] )
+        then
+                as_active_ips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "autoscaler" "${CLOUDHOST}"`"
+        elif ( [ "${PRODUCTION}" = "1" ] && [ "${NO_AUTOSCALERS}" -eq "1" ] )
+        then
+                as_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "autoscaler" "${CLOUDHOST}"`"
+        fi
+        ws_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "webserver" "${CLOUDHOST}"`"
+        db_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "database" "${CLOUDHOST}"`"
+elif ( [ "${BUILD_MACHINE_VPC}" = "1" ] )
+then
+        if ( [ "${PRODUCTION}" = "1" ] && [ "${NO_AUTOSCALERS}" -gt "1" ] )
+        then
+                as_active_ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "autoscaler" "${CLOUDHOST}"`"
+        elif ( [ "${PRODUCTION}" = "1" ] && [ "${NO_AUTOSCALERS}" -eq "1" ] )
+        then
+                as_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "autoscaler" "${CLOUDHOST}"`"
+        fi
+        ws_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "webserver" "${CLOUDHOST}"`"
+        db_active_ip="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "database" "${CLOUDHOST}"`"
+fi
+
+AUTOSCALER_PUBLIC_KEYS="${BUILD_HOME}/keys/${CLOUDHOST}/${BUILD_IDENTIFIER}/autoscaler_keys"
+OPTIONS="-o ConnectTimeout=10 -o ConnectionAttempts=5 -o UserKnownHostsFile=${AUTOSCALER_PUBLIC_KEYS} -o StrictHostKeyChecking=yes "
+
+if ( [ "${as_active_ips}" != "" ] )
+then
+        for ip in `/bin/echo ${as_active_ips} | /bin/sed 's/:/ /g'`
+        do
+                /usr/bin/scp ${OPTIONS} -P ${SSH_PORT} ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/webserver_configuration_settings.dat ${SERVER_USER}@${ip}:/home/${SERVER_USER}/.ssh/webserver_configuration_settings.dat >/dev/null 2>&1        
+                /usr/bin/scp ${OPTIONS} -P ${SSH_PORT} ${BUILD_HOME}/builddescriptors/buildstylesscp.dat ${SERVER_USER}@${ip}:/home/${SERVER_USER}/.ssh/buildstyles.dat >/dev/null 2>&1    
+        done
+elif ( [ "${as_active_ip}" != "" ] )
+then
+        /usr/bin/scp ${OPTIONS} -P ${SSH_PORT} ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/webserver_configuration_settings.dat ${SERVER_USER}@${as_active_ip}:/home/${SERVER_USER}/.ssh/webserver_configuration_settings.dat >/dev/null 2>&1        
+        /usr/bin/scp ${OPTIONS} -P ${SSH_PORT} ${BUILD_HOME}/builddescriptors/buildstylesscp.dat ${SERVER_USER}@${as_active_ip}:/home/${SERVER_USER}/.ssh/buildstyles.dat >/dev/null 2>&1    
+fi
+
+if ( [ "${PRODUCTION}" = "1" ] )
+then
+        status "Autoscaler, webserver and database built correctly....."
+elif ( [ "${DEVELOPMENT}" = "1" ] )
+then
+        status "Webserver and database built correctly....."
+fi
+
+. ${BUILD_HOME}/providerscripts/security/firewall/TightenDBaaSFirewall.sh
+/bin/touch ${BUILD_HOME}/runtimedata//PRIME_FIREWALL
+cloudhost_holder="${CLOUDHOST}"
+. ${BUILD_HOME}/providerscripts/security/firewall/TightenBuildMachineFirewall.sh
+export CLOUDHOST="${cloudhost_holder}"
+
+if ( [ "${BUILD_ARCHIVE_CHOICE}" != "virgin" ] )
+then
+        . ${BUILD_HOME}/providerscripts/application/SetApplicationConfig.sh
+fi
+       
+##Do the build finalisation procedures
+. ${BUILD_HOME}/buildscripts/FinaliseBuildProcessing.sh
