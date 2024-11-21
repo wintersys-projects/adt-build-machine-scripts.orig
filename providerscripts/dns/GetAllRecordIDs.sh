8f45a44ef3de7b4675e5367f9fc90dc46e890f76
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-21 21:59:16 +0000

    Update GetAllRecordIDs.sh

--- providerscripts/dns/GetAllRecordIDs.sh
+++ providerscripts/dns/GetAllRecordIDs.sh
@@ -74,5 +74,5 @@ dns="${5}"
 if ( [ "${dns}" = "vultr" ] )
 then
 	export VULTR_API_KEY="${authkey}"
-	/usr/bin/vultr dns record list nuocial.uk -o json | /usr/bin/jq -r '.records[] | select (.name == "'${subdomain}'").id'
+	/usr/bin/vultr dns record list ${domainurl} -o json | /usr/bin/jq -r '.records[] | select (.name == "'${subdomain}'").id'
 fi
