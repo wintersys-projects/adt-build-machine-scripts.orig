270b17cf69fc588ba0c7478c0e299c2149f134e7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-22 23:17:11 +0000

    Update NumberOfServers.sh

--- providerscripts/server/NumberOfServers.sh
+++ providerscripts/server/NumberOfServers.sh
@@ -23,7 +23,8 @@
 server_type="${1}"
 cloudhost="${2}"
 
-BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+BUILD_HOME="`/bin/cat /home/buildhome.dat`" 
+BUILD_IDENTIFIER="`/bin/cat ${BUILD_HOME}/runtimedata/ACTIVEBUILDIDENTIFIER`"
 
 if ( [ "${cloudhost}" = "digitalocean" ] )
 then
