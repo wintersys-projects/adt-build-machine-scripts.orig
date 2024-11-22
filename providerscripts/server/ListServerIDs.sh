4fade50121d584e5f461c6a57c08bfa5939c3076
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-22 23:21:18 +0000

    Update ListServerIDs.sh

--- providerscripts/server/ListServerIDs.sh
+++ providerscripts/server/ListServerIDs.sh
@@ -33,7 +33,7 @@ fi
 
 if ( [ "${cloudhost}" = "exoscale" ] )
 then
- 	zone="`/bin/cat ${BUILD_HOME}/runtimedata/${cloudhost}/CURRENTREGION`"
+ 	zone="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/CURRENTREGION`"
 	/usr/bin/exo compute instance list --zone ${zone} -O json | /usr/bin/jq -r '.[] | select (.name | contains("'${server_type}'")).id'
 fi
 
