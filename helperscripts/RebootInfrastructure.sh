e2d3afe420d2b0c7cc3463a339aa598418758235
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-22 19:22:56 +0000

    Update RebootInfrastructure.sh

--- helperscripts/RebootInfrastructure.sh
+++ helperscripts/RebootInfrastructure.sh
@@ -67,21 +67,21 @@ fi
 /bin/echo "Please enter the name of the build of the server you wish to connect with"
 read BUILD_IDENTIFIER
 
-if ( [ -f ${BUILD_HOME}/runtimedata/ips/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
+if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
 then
 	autoscalerips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "autoscaler" ${CLOUDHOST} ${BUILD_HOME}`"
 else
 	autoscalerips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "autoscaler" ${CLOUDHOST} ${BUILD_HOME}`"
 fi
 
-if ( [ -f ${BUILD_HOME}/runtimedata/ips/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
+if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
 then
 	webserverips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "webserver" ${CLOUDHOST} ${BUILD_HOME}`"
 else
 	webserverips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "webserver" ${CLOUDHOST} ${BUILD_HOME}`"
 fi
 
-if ( [ -f ${BUILD_HOME}/runtimedata/ips/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
+if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
 then
 	databaseips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "database" ${CLOUDHOST} ${BUILD_HOME}`"
 else
