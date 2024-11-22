17196036045fde878505babe5b7384625bfcde6e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-22 19:35:40 +0000

    Update GetApplicationDefaultConfig.sh

--- providerscripts/application/joomla/GetApplicationDefaultConfig.sh
+++ providerscripts/application/joomla/GetApplicationDefaultConfig.sh
@@ -23,11 +23,11 @@
 #####################################################################################
 #set -x
  
-while ( [ ! -f ${BUILD_HOME}/buildconfiguration/configuration.php.default ] )
+while ( [ ! -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/configuration.php.default ] )
 do
-	${BUILD_HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh ${WEBSITE_URL} configuration.php.default  ${BUILD_HOME}/buildconfiguration
- 	status "Couldn't find the joomla configuration.php in the datastore (it hasn't been generated yet) will try again shortly"
-  	/bin/sleep 10
+        ${BUILD_HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh ${WEBSITE_URL} configuration.php.default  ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}
+        status "Couldn't find the joomla configuration.php in the datastore (it hasn't been generated yet) will try again shortly"
+        /bin/sleep 10
 done
 
 status "Have found the joomla configuration.php in the datastore can continue now..."
