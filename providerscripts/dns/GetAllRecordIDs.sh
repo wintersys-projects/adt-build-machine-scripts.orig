ea546e1cc50dcdc9591020c11e3de52156f1f2d8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-22 15:57:59 +0000

    Update GetAllRecordIDs.sh

--- providerscripts/dns/GetAllRecordIDs.sh
+++ providerscripts/dns/GetAllRecordIDs.sh
@@ -62,7 +62,7 @@ dns="${5}"
 
 if ( [ "${dns}" = "linode" ] )
 then
-	domain_id="`/usr/local/bin/linode-cli --json domains list | /usr/bin/jq -r --arg tmp_domain_url "${domain_url}" '(.[] | select(.domain | contains($tmp_domain_url)) | .id)'`"
+	domain_id="`/usr/local/bin/linode-cli --json linodes list | /usr/bin/jq -r '.[] | select (.domain | contains("'${domain_url}'")).id'`"
 	/usr/local/bin/linode-cli --json domains records-list ${domain_id}  | /usr/bin/jq -r --arg tmp_subdomain "${subdomain}" '(.[] | select(.name | contains($tmp_subdomain)) | .id)'
 fi
 
