0660573dd6167d5bea4ec16101bc7231708c1e1d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-22 23:16:28 +0000

    Update DestroyServer.sh

--- providerscripts/server/DestroyServer.sh
+++ providerscripts/server/DestroyServer.sh
@@ -27,8 +27,8 @@ status () {
 server_ip="${1}"
 cloudhost="${2}"
 
+BUILD_HOME="`/bin/cat /home/buildhome.dat`" 
 BUILD_IDENTIFIER="`/bin/cat ${BUILD_HOME}/runtimedata/ACTIVEBUILDIDENTIFIER`"
-
 
 if ( [ "${cloudhost}" = "digitalocean" ] )
 then
