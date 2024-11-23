9a83003d48a9c059e9acc4057f29de1592b0ed13
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-23 21:23:57 +0000

    Update RebootInfrastructure.sh

--- helperscripts/RebootInfrastructure.sh
+++ helperscripts/RebootInfrastructure.sh
@@ -56,13 +56,7 @@ fi
 /bin/echo "What is the build identifier you want to connect to?"
 /bin/echo "You have these builds to choose from: "
 
-if ( [ -d ${BUILD_HOME}/buildconfiguration/${CLOUDHOST} ] )
-then
-        /bin/ls ${BUILD_HOME}/buildconfiguration/${CLOUDHOST} | /bin/grep -v 'credentials'
-else
-        /bin/echo "No eligible builds found"
-        exit
-fi
+/bin/ls ${BUILD_HOME}/runtimedata/${CLOUDHOST}
 
 /bin/echo "Please enter the name of the build of the server you wish to connect with"
 read BUILD_IDENTIFIER
