031d07dcc1cc12d3980e54f28dd585f0f059047e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-23 17:32:37 +0000

    Update CopyFromAutoscaler.sh

--- helperscripts/CopyFromAutoscaler.sh
+++ helperscripts/CopyFromAutoscaler.sh
@@ -108,7 +108,7 @@ then
     exit
 fi
 
-SERVER_USERNAME="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER`"
+SERVER_USER="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER`"
 SSH_PORT="`/bin/grep SSH_PORT ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/build_environment | /bin/sed 's/"//g' | /usr/bin/awk -F'=' '{print $NF}'`"
 AUTOSCALER_PUBLIC_KEYS="${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/autoscaler_${AS_IP}keys"
 
