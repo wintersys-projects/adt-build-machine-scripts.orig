1fe84f58992fc7fe3fb0e20375512c40c5858af9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-24 00:36:24 +0000

    Create DisplayLoggingStreams.sh

---
+++ helperscripts/DisplayLoggingStreams.sh
@@ -0,0 +1,39 @@
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+
+/bin/echo "Which cloudhost do you want to view logs for DigitalOcean (do), Exoscale (exo), Linode (lin) or Vultr (vul)"
+/bin/echo "Please type one of do, exo, lin, vul"
+read response
+
+if ( [ "${response}" = "do" ] )
+then
+  CLOUDHOST="digitalocean"
+elif ( [ "${CLOUDHOST}" = "exo" ] )
+then
+  CLOUDHOST="exoscale"
+elif ( [ "${CLOUDHOST}" = "lin" ] )
+then
+  CLOUDHOST="linode" 
+elif ( [ "${CLOUDHOST}" = "vul" ] )
+then
+  CLOUDHOST="vultr"
+fi
+
+/bin/echo "What is the build identifier you want to connect to?"
+/bin/echo "You have these builds to choose from: "
+
+/bin/ls ${BUILD_HOME}/runtimedata/${CLOUDHOST}
+
+/bin/echo "Please enter the name of the build of the server you wish to connect with"
+read BUILD_IDENTIFIER
+
+/bin/echo "Do you want out (1) or err (2)"
+read response
+
+if ( [ "${response}" = "1" ] )
+then
+
+elif ( [ "${respose}" = "2" ] )
+then
+
+fi
+
