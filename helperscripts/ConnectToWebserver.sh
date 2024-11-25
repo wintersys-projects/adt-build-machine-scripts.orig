02d1140b337e17587f4ff209b932e1879c14950a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-25 18:48:42 +0000

    Update ConnectToWebserver.sh

--- helperscripts/ConnectToWebserver.sh
+++ helperscripts/ConnectToWebserver.sh
@@ -48,6 +48,7 @@ elif ( [ "${response}" = "4" ] )
 then
         CLOUDHOST="vultr"
         token_to_match="webserver"
+        export VULTR_API_KEY="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/TOKEN`"
 else
         /bin/echo "Unrecognised  cloudhost. Exiting ...."
         exit
