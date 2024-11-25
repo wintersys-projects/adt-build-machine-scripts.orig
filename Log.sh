57c761b19d339c2a9e827f63c63f414e2bfe0ad6
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-25 14:42:53 +0000

    Update Log.sh

--- Log.sh
+++ Log.sh
@@ -1,2 +1,8 @@
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 ${BUILD_HOME}/helperscripts/DisplayLoggingStreams.sh
+
+#You can set yourself up with oneliners to access particular log or error streams this will provide you with rapid access to your build streams
+#You should comment out the interactive call above and comment in a command like the ones shown below which are appropriate for you
+#${BUILD_HOME}/helperscripts/DisplayLoggingStreams.sh vultr crew c 2 #cat the error stream for vultr with build identifer "crew"
+#${BUILD_HOME}/helperscripts/DisplayLoggingStreams.sh linode crew t 1 #tail the output stream for linode with build identifier "crew"
+#${BUILD_HOME}/helperscripts/DisplayLoggingStreams.sh digitalocean crew v 1 #edit the output stream for digitalocean with identifier "crew"
