19fbd6579154897736dd293c052eab2546179873
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-25 18:50:17 +0000

    Update CopyToDatabase.sh

--- helperscripts/CopyToDatabase.sh
+++ helperscripts/CopyToDatabase.sh
@@ -64,6 +64,7 @@ elif ( [ "${response}" = "4" ] )
 then
 	CLOUDHOST="vultr"
 	token_to_match="database"
+	export VULTR_API_KEY="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/TOKEN`"
 fi
 
 /bin/echo "What is the build identifier you want to connect to?"
