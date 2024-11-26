1ad6110e8a211ab4ec0b95d32fd4e5a9ead779b2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-26 16:23:19 +0000

    Update CopyToWebserver.sh

--- helperscripts/CopyToWebserver.sh
+++ helperscripts/CopyToWebserver.sh
@@ -52,19 +52,15 @@ read response
 if ( [ "${response}" = "1" ] )
 then
 	CLOUDHOST="digitalocean"
-	token_to_match="webserver"
 elif ( [ "${response}" = "2" ] )
 then
 	CLOUDHOST="exoscale"
-	token_to_match="webserver"
 elif ( [ "${response}" = "3" ] )
 then
 	CLOUDHOST="linode"
-	token_to_match="webserver"
 elif ( [ "${response}" = "4" ] )
 then
 	CLOUDHOST="vultr"
-	token_to_match="webserver"
 	export VULTR_API_KEY="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/TOKEN`"
 fi
 
@@ -76,6 +72,7 @@ fi
 /bin/echo "Please enter the name of the build of the server you wish to connect with"
 read BUILD_IDENTIFIER
 
+token_to_match="ws-`/bin/grep 'REGION=' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/build_environment | /usr/bin/awk -F'=' '{print $NF}'`-${BUILD_IDENTIFIER}"
 /bin/echo "${BUILD_IDENTIFIER}" > ${BUILD_HOME}/runtimedata/ACTIVE_BUILD_IDENTIFIER
 
 if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
