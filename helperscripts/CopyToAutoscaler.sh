2192912a98f2ed7ee27f8a3eeb72abcf7a4d09b8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-26 16:21:07 +0000

    Update CopyToAutoscaler.sh

--- helperscripts/CopyToAutoscaler.sh
+++ helperscripts/CopyToAutoscaler.sh
@@ -51,19 +51,15 @@ read response
 if ( [ "${response}" = "1" ] )
 then
 	CLOUDHOST="digitalocean"
-	token_to_match="autoscaler"
 elif ( [ "${response}" = "2" ] )
 then
 	CLOUDHOST="exoscale"    
-	token_to_match="autoscaler"
 elif ( [ "${response}" = "3" ] )
 then
 	CLOUDHOST="linode"
-	token_to_match="autoscaler"
 elif ( [ "${response}" = "4" ] )
 then
 	CLOUDHOST="vultr"
-	token_to_match="autoscaler"
 	export VULTR_API_KEY="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/TOKEN`"
 fi
 
@@ -75,6 +71,7 @@ fi
 /bin/echo "Please enter the name of the build of the server you wish to connect with"
 read BUILD_IDENTIFIER
 
+token_to_match="as-`/bin/grep 'REGION=' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/build_environment | /usr/bin/awk -F'=' '{print $NF}'`-${BUILD_IDENTIFIER}"
 /bin/echo "${BUILD_IDENTIFIER}" > ${BUILD_HOME}/runtimedata/ACTIVE_BUILD_IDENTIFIER
 
 if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
