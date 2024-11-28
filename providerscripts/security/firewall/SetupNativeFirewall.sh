ce19e67cdfea57404e8d28cccb2f454ef05da70a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-28 10:58:33 +0000

    Update SetupNativeFirewall.sh

--- providerscripts/security/firewall/SetupNativeFirewall.sh
+++ providerscripts/security/firewall/SetupNativeFirewall.sh
@@ -440,16 +440,24 @@ then
                                         /usr/bin/vultr firewall group delete ${firewall_id}
                                 done
                         fi
-                        
-                        firewall_id="`/usr/bin/vultr firewall group create -o json | /usr/bin/jq -r '.firewall_group.id'`"  
-                        /usr/bin/vultr firewall group update ${firewall_id} --description "adt-autoscaler-${BUILD_IDENTIFIER}"
-                        
-                        firewall_id="`/usr/bin/vultr firewall group create -o json | /usr/bin/jq -r '.firewall_group.id'`"  
-                        /usr/bin/vultr firewall group update ${firewall_id} --description "adt-webserver-${BUILD_IDENTIFIER}"
 
-                        firewall_id="`/usr/bin/vultr firewall group create -o json | /usr/bin/jq -r '.firewall_group.id'`"  
-                        /usr/bin/vultr firewall group update ${firewall_id} --description "adt-database-${BUILD_IDENTIFIER}"
-
+                        if ( [ "`/usr/bin/vultr -o json firewall group list | /usr/bin/jq -r '.firewall_groups[] | select(.description | contains ("adt-autoscaler-'${BUILD_IDENTIFIER}'")).id'`" = "" ] )
+                        then        
+                                firewall_id="`/usr/bin/vultr firewall group create -o json | /usr/bin/jq -r '.firewall_group.id'`"  
+                                /usr/bin/vultr firewall group update ${firewall_id} --description "adt-autoscaler-${BUILD_IDENTIFIER}"
+                        fi
+                        
+                        if ( [ "`/usr/bin/vultr -o json firewall group list | /usr/bin/jq -r '.firewall_groups[] | select(.description | contains ("adt-webserver-'${BUILD_IDENTIFIER}'")).id'`" = "" ] )
+                        then
+                                firewall_id="`/usr/bin/vultr firewall group create -o json | /usr/bin/jq -r '.firewall_group.id'`"  
+                                /usr/bin/vultr firewall group update ${firewall_id} --description "adt-webserver-${BUILD_IDENTIFIER}"
+                        fi
+                        
+                        if ( [ "`/usr/bin/vultr -o json firewall group list | /usr/bin/jq -r '.firewall_groups[] | select(.description | contains ("adt-database-'${BUILD_IDENTIFIER}'")).id'`" = "" ] )
+                        then
+                                firewall_id="`/usr/bin/vultr firewall group create -o json | /usr/bin/jq -r '.firewall_group.id'`"  
+                                /usr/bin/vultr firewall group update ${firewall_id} --description "adt-database-${BUILD_IDENTIFIER}"
+                        fi
                 fi
         fi
 fi
