9d2c6825c21c3392109196cf0735ebec21a54cba
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-02 13:24:46 +0000

    Update HasInstanceRunning.sh

--- providerscripts/server/HasInstanceRunning.sh
+++ providerscripts/server/HasInstanceRunning.sh
@@ -32,8 +32,8 @@ then
 fi
 if ( [ "${cloudhost}" = "exoscale" ] )
 then
-	zone="`/bin/cat ${BUILD_HOME}/runtimedata/${cloudhost}/${BUILD_IDENTIFIER}/CURRENTREGION`"
-	/usr/bin/exo compute instance list --zone ${zone} -O text | /bin/grep "${server_type}" 
+ 	zone="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'REGION'`"
+ 	/usr/bin/exo compute instance list --zone ${zone} -O json | /usr/bin/jq -r '.[] | select (.name | contains("'${server_type}'")).id' 
 fi
 
 if ( [ "${cloudhost}" = "linode" ] )
