642663b80beaec04bebcdad75b28dd007afc245f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-03 10:10:41 +0000

    Update GetSSHKeyID.sh

--- providerscripts/security/GetSSHKeyID.sh
+++ providerscripts/security/GetSSHKeyID.sh
@@ -25,7 +25,7 @@ cloudhost="${2}"
 
 if ( [ "${cloudhost}" = "digitalocean" ] )
 then
-	/usr/local/bin/doctl compute ssh-key list -o json | /usr/bin/jq '.[] | select (.name == "'${key_name}'").id'
+	/usr/local/bin/doctl compute ssh-key list -o json | /usr/bin/jq '.[] | select (.name == "'${public_key_name}'").id'
 fi
 
 if ( [ "${cloudhost}" = "exoscale" ] )
