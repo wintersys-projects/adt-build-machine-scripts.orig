0caaeacaa3371877608fea1589f9d3c6f3c531dd
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-04 11:49:45 +0000

    Update ExecuteOnAutoscaler.sh

--- helperscripts/ExecuteOnAutoscaler.sh
+++ helperscripts/ExecuteOnAutoscaler.sh
@@ -139,8 +139,12 @@ else
         ALGORITHM="`/bin/grep ALGORITHM ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/build_environment | /bin/sed 's/"//g' | /usr/bin/awk -F'=' '{print $NF}'`"
 fi
 
+start=`/bin/date +%s`
 /usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${AUTOSCALER_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USERNAME}@${AUTOSCALER_IP} "${command}"
-if ( [ "$?" != "0" ] )
+end=`/bin/date +%s`
+runtime="`/usr/bin/expr ${end} - ${start}`"
+
+if ( [ "${runtime}" -lt "3" ] )
 then
 	/bin/echo "#####################################################################################################################################################################"
 	/bin/echo "Do you want to initiate a fresh ssh key scan (might be necessary if you can't connect) or  do you want to use previously generated keys"
