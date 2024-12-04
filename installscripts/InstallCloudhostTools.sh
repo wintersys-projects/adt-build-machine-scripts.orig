ec1704e5dd36e8b5e129479d81aa2a69890091d9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-04 09:47:45 +0000

    Update InstallCloudhostTools.sh

--- installscripts/InstallCloudhostTools.sh
+++ installscripts/InstallCloudhostTools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ######################################################################################
-# Description: This script will install the CLOUDHOST tools for the provider you have
+# Description: This script will install the cloudhost tools for the provider you have
 # selected. These tools will give API access to the provider which can then be used by
 # the other scripts to manipulate resourced on that providers infrastructure.
 # Date: 07-11-2016
@@ -20,42 +20,41 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ######################################################################################
 ######################################################################################
-#set -x
+set -x
 
-CLOUDHOST="${1}"
-BUILDOS="${2}"
+cloudhost="${1}"
+buildos="${2}"
 
-if ( [ "${CLOUDHOST}" = "digitalocean" ] )
+if ( [ "${cloudhost}" = "digitalocean" ] )
 then
-	if ( [ ! -f /usr/local/bin/doctl ] )
-	then
-		${BUILD_HOME}/installscripts/InstallDoctl.sh "${BUILDOS}"
-	fi
+        if ( [ ! -f /usr/local/bin/doctl ] )
+        then
+                ${BUILD_HOME}/installscripts/InstallDoctl.sh "${buildos}"
+        fi
 fi
 
-if ( [ "${CLOUDHOST}" = "exoscale" ] )
+if ( [ "${cloudhost}" = "exoscale" ] )
 then
-	if ( [ ! -f /usr/bin/exo ] )
-	then
-		${BUILD_HOME}/installscripts/InstallExo.sh "${BUILDOS}"
-	fi
+        if ( [ ! -f /usr/bin/exo ] )
+        then
+                ${BUILD_HOME}/installscripts/InstallExo.sh "${buildos}"
+        fi
 fi
 
-if ( [ "${CLOUDHOST}" = "linode" ] )
+if ( [ "${cloudhost}" = "linode" ] )
 then
-	if ( [ ! -f /usr/local/bin/linode-cli ] )
-	then
-		${BUILD_HOME}/installscripts/InstallLinodeCLI.sh "${BUILDOS}"
-	fi
+        if ( [ ! -f /usr/local/bin/linode-cli ] )
+        then
+                ${BUILD_HOME}/installscripts/InstallLinodeCLI.sh "${buildos}"
+        fi
 fi
 
-if ( [ "${CLOUDHOST}" = "vultr" ] )
+if ( [ "${cloudhost}" = "vultr" ] )
 then
-	if ( [ ! -f /usr/bin/vultr ] )
-	then
-		${BUILD_HOME}/installscripts/InstallSudo.sh "${BUILDOS}"
-		#${BUILD_HOME}/installscripts/InstallGo.sh "${BUILDOS}"        
-		${BUILD_HOME}/installscripts/InstallVultrCLI.sh "${BUILDOS}"        
-	fi
+        if ( [ ! -f /usr/bin/vultr ] )
+        then
+                ${BUILD_HOME}/installscripts/InstallSudo.sh "${buildos}"
+                ${BUILD_HOME}/installscripts/InstallVultrCLI.sh "${buildos}"        
+        fi
 fi
 
