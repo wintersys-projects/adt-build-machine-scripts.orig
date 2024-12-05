0e52f2177b18be558d526da567ee380f722791ad
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-05 12:42:17 +0000

    Update GenerateSnapshot.sh

--- providerscripts/server/GenerateSnapshot.sh
+++ providerscripts/server/GenerateSnapshot.sh
@@ -1,5 +1,3 @@
-
-
 cloudhost="${1}"
 machine_name="${2}"
 default_user="${3}"
@@ -9,9 +7,9 @@ BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 
 if ( [ "${cloudhost}" = "exoscale" ] )
 then
-  REGION_ID="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/CURRENTREGION`"
-  machine_id="`${BUILD_HOME}/providerscripts/server/ListServerIDs.sh ${machine_name} ${CLOUDHOST}`"  
+  REGION_ID="`/bin/cat ${BUILD_HOME}/runtimedata/${cloudhost}/CURRENTREGION`"
+  machine_id="`${BUILD_HOME}/providerscripts/server/ListServerIDs.sh ${machine_name} ${cloudhost}`"  
   /usr/bin/exo compute instance snapshot create -z ${REGION_ID} ${machine_id}
   snapshot_id="`/usr/bin/exo -O json  compute instance snapshot list  | /usr/bin/jq -r '.[] | select (.instance == "'${machine_name}'") | select (.zone == "'${REGION_ID}'").id'`"
-  /usr/bin/exo compute instance-template register --boot-mode legacy --disable-password --from-snapshot ${snapshot_id} --zone ${REGION_ID} --username ${DEFAULT_USER} ${machine_name}
+  /usr/bin/exo compute instance-template register --boot-mode legacy --disable-password --from-snapshot ${snapshot_id} --zone ${REGION_ID} --username ${default_user} ${machine_name}
 fi
