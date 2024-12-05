863879ba5404b3be2c4259b41a57b9a107078d70
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-05 15:06:56 +0000

    Update GenerateSnapshot.sh

--- providerscripts/server/GenerateSnapshot.sh
+++ providerscripts/server/GenerateSnapshot.sh
@@ -6,6 +6,13 @@ default_user="${3}"
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 BUILD_IDENTIFIER="`/bin/cat ${BUILD_HOME}/runtimedata/ACTIVE_BUILD_IDENTIFIER`"
 
+if ( [ "${cloudhost}" = "digitalocean" ] )
+then
+        machine_id="`${BUILD_HOME}/providerscripts/server/ListServerIDs.sh ${machine_type} ${cloudhost}`"  
+        autoscaler_name="`/usr/local/bin/doctl compute droplet list -o json | /usr/bin/jq -r '.[] | select (.name | contains ("'${machine_type}'")).name'`"
+        /usr/local/bin/doctl compute droplet-action snapshot --snapshot-name "${autoscaler_name}" ${autoscaler_id}
+fi
+
 if ( [ "${cloudhost}" = "exoscale" ] )
 then
   REGION_ID="`/bin/cat ${BUILD_HOME}/runtimedata/${cloudhost}/${BUILD_IDENTIFIER}/CURRENTREGION`"
