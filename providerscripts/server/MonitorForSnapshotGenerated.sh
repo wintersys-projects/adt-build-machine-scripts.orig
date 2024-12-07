ffeebabc4261cb7d7c1795323275912bae1fc732
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-07 18:24:30 +0000

    Update MonitorForSnapshotGenerated.sh

--- providerscripts/server/MonitorForSnapshotGenerated.sh
+++ providerscripts/server/MonitorForSnapshotGenerated.sh
@@ -1,5 +1,7 @@
 
 
+rnd="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER | /usr/bin/fold -w 4 | /usr/bin/head -n 1`"
+
 
 if ( [ "${CLOUDHOST}" = "linode" ] )
 then
@@ -10,7 +12,7 @@ then
   do
         for prefix in ${prefixes}
         do
-                result="`/usr/local/bin/linode-cli images list --json | /usr/bin/jq -r '.[] | select ( .label | contains ("'${prefix}${REGION}-${BUILD_IDENTIFIER}-'")).status'`" 
+                result="`/usr/local/bin/linode-cli images list --json | /usr/bin/jq -r '.[] | select ( .label | contains ("'${prefix}${REGION}-${BUILD_IDENTIFIER}-${rnd}'")).status'`" 
                 if ( [ "${result}" = "available" ] )
                 then
                         prefixes="`/bin/echo ${prefixes} | /bin/sed "s/${prefix}//g"`"
