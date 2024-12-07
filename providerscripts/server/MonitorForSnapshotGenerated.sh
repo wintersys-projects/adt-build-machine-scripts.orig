44e5e3579ead26f2215abf9c158bfc2198613c40
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-07 13:23:52 +0000

    Update MonitorForSnapshotGenerated.sh

--- providerscripts/server/MonitorForSnapshotGenerated.sh
+++ providerscripts/server/MonitorForSnapshotGenerated.sh
@@ -1,3 +1,21 @@
 
 
-linode-cli images list --json | /usr/bin/jq -r '.[] | select ( .label | contains ("as-'${REGION}-${BUILD_IDENTIFIER}-${RND}'")).status
+
+if ( [ "${CLOUDHOST}" = "linode" ] )
+then
+  status "Monitoring for your snapshots to have fully, might take a minute, please wait"
+  prefixes="as- ws- db-"
+
+  while ( "`/bin/echo ${prefixes} | /bin/sed 's/ //g'`" != "" )
+  do
+    for prefix in ${prefixes}
+    do
+      result="`linode-cli images list --json | /usr/bin/jq -r '.[] | select ( .label | contains ("'${REGION}-${BUILD_IDENTIFIER}-${RND}'")).status`" = "available")
+      if ( [ "${result}" = "available" ] )
+      then
+        prefixes="`/bin/echo ${prefixes} | /bin/sed "s/${prefix}//g"`"
+      fi
+    do
+  done
+fi
+status "All napshots generated"
