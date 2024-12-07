ca0b501aa452ca24573f4a27af3ff7010278f6ed
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-07 13:31:52 +0000

    Update MonitorForSnapshotGenerated.sh

--- providerscripts/server/MonitorForSnapshotGenerated.sh
+++ providerscripts/server/MonitorForSnapshotGenerated.sh
@@ -6,16 +6,16 @@ then
   status "Monitoring for your snapshots to have fully, might take a minute, please wait"
   prefixes="as- ws- db-"
 
-  while ( "`/bin/echo ${prefixes} | /bin/sed 's/ //g'`" != "" )
+  while ( [ "`/bin/echo ${prefixes} | /bin/sed 's/ //g'`" != "" ] )
   do
-    for prefix in ${prefixes}
-    do
-      result="`linode-cli images list --json | /usr/bin/jq -r '.[] | select ( .label | contains ("'${REGION}-${BUILD_IDENTIFIER}-${RND}'")).status`" = "available")
-      if ( [ "${result}" = "available" ] )
-      then
-        prefixes="`/bin/echo ${prefixes} | /bin/sed "s/${prefix}//g"`"
-      fi
-    do
+        for prefix in ${prefixes}
+        do
+                result="`/usr/local/bin/linode-cli images list --json | /usr/bin/jq -r '.[] | select ( .label | contains ("'${prefix}${REGION}-${BUILD_IDENTIFIER}-${RND}'")).status'`" 
+                if ( [ "${result}" = "available" ] )
+                then
+                        prefixes="`/bin/echo ${prefixes} | /bin/sed "s/${prefix}//g"`"
+                fi
+        done
   done
+  status "All snapshots generated"
 fi
-status "All napshots generated"
